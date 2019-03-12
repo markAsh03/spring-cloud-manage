41556dfa589bdbbcb65d39c28f9d1f0dc832d798
MarkAsh03 <muanan@cic.cn>
2019-03-12 09:15:59 +0800

    cloud eureka 单节点

---
+++ cloud-eureka/src/test/java/com/markash/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markash.cloudeureka;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class CloudEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
